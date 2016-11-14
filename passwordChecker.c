729768939051b44932d6ab8cf3f27646120109d7
Thomas Parker <12tpjp@queensu.ca>
2016-11-14 14:25:10 -0800

    thirdquestion

--- passwordChecker.c
+++ passwordChecker.c
@@ -3,6 +3,7 @@
 #include <termios.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char character()
 {
@@ -14,22 +15,46 @@ char character()
 	tcsetattr( STDIN_FILENO, TCSANOW, &new);
 	ch = getchar();
 	tcsetattr( STDIN_FILENO, TCSANOW, &old);
-	printf("%d",ch);
 	return ch;
 }
 
 int main(){
+	int quality = 0;
 	char pswrd[24];
 	int i = 0;
-	character();
-	/*while(1){
-		pswrd[i]=character();
-		///if (pswrd[i] == "\n"){
-		//	break;
-		///	printf("1");
+	while(1){
+		if ((pswrd[i]=character())!='\n'){
+			i++;
 		}
-		i++;
-	}*/
-	//printf("%d Hello my baby hello my darling, \n hello my ragtime gal \n", pswrd[1]);
+		else{
+			break;
+		}
+	}
+	if (i >=8){
+		quality += 1;
+	}
+	size_t len = strlen(pswrd);
+	size_t dif = strcspn(pswrd,"@#$%&*+-=");
+	if (len != dif){
+		quality+=1;
+	}
+	dif = strcspn(pswrd,"1234567890");
+	if (len != dif){
+		quality+=1;
+	}
+	switch(quality) {
+		case 0:
+			printf("Terrible\n");
+			break;
+		case 1:
+			printf("Weak\n");
+			break;
+		case 2:
+			printf("Moderate\n");
+			break;
+		case 3:
+			printf("Strong\n");
+			break;
+	}
 }
 		
